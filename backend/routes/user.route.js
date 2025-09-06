052854b7e0fe278bbbbf4389fc01a250a984e954
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-09-06 16:54:15 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMe, login, logout, register } from "../controllers/auth.controller.js";
+import { authMe, login, logout, register,adminLogin } from "../controllers/auth.controller.js";
 import {protect} from "../middlewares/auth.middlerware.js";
 import { getProfile, updateProfile } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
@@ -15,5 +15,8 @@ router.get("/profile", protect, getProfile);
 // Update user profile
 router.put("/profile", protect, upload.single("profilePhoto"), updateProfile);
 router.get("/me",authMe);
+
+//admin Login
+router.post("/admin/login",adminLogin);
 
 export default router;
