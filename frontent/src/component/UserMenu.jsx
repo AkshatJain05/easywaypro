384d669f013ecf48495a29a2084679b0f7886f3d
Akshat Jain <156729933+AkshatJain05@users.noreply.github.com>
2025-09-07 18:50:43 +0530

    Update UserMenu.jsx

--- frontent/src/component/UserMenu.jsx
+++ frontent/src/component/UserMenu.jsx
@@ -71,7 +71,7 @@ export default function UserMenu() {
 
   // 🔹 User is logged in → show avatar + dropdown
   return (
-    <div className="relative z-160">
+    <div className="relative">
      <button
   ref={btnRef}
   onClick={() => setOpen((v) => !v)}
@@ -94,7 +94,7 @@ export default function UserMenu() {
       {open && (
         <div
           ref={menuRef}
-          className="absolute right-0 mt-2 w-56 rounded-xl bg-white dark:bg-gray-900 shadow-lg border border-gray-200 dark:border-gray-700 ring-1 ring-black/5 backdrop-blur"
+          className="absolute z-120 right-0 mt-2 w-56 rounded-xl bg-white dark:bg-gray-900 shadow-lg border border-gray-200 dark:border-gray-700 ring-1 ring-black/5 backdrop-blur"
         >
           <div className="px-4 py-3 border-b border-gray-100 dark:border-gray-700">
             <p className="text-xs text-gray-500 dark:text-gray-400">Signed in as</p>
